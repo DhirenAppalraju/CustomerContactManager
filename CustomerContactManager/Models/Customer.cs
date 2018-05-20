38281fe9df333d55d2cf36389ca37b06468745d7
Dhiren Appalraju <appalraju@gmail.com>
2018-05-20 10:28:18 +0200

    Updated packages, created models and data context

---
+++ CustomerContactManager/Models/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerContactManager.Models
+{
+    public class BaseCustomer
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display (Name = "Name")]
+        public string Name { get; set; }
+    }
+
+    public class Customer : BaseCustomer
+    {
+        [Required]
+        [Display(Name = "Latitude")]
+        public string Latitude { get; set; }
+
+        [Required]
+        [Display(Name = "Longitude")]
+        public string Longitude { get; set; }
+
+        //public List<CustomerContact> Contacts { get; set; }
+    }
+
+    public class CustomerContact : BaseCustomer
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "ContactNumber")]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        [Display(Name = "CustomerId")]
+        public string CustomerId { get; set; }
+    }
+}
